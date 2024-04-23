e195db923c748c03dadb459d12cb4a5e04af67c3
june lee <melodysdreamj@gmail.com>
2024-04-23 11:04:56 +0900

    ₩

--- lib/app/_/_/interaction/view.blueprint/page/basic_keyboard/_/view.dart
+++ lib/app/_/_/interaction/view.blueprint/page/basic_keyboard/_/view.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:keyboard_attachable/keyboard_attachable.dart';
 
 import '../../../../../../../../main.dart';
-import '../appbar/view.dart';
-import '../chat_card/view.dart';
-import '../drawer/view.dart';
 import '_/state_child.dart';
 import '_/state_mother.dart';
 
@@ -31,7 +28,7 @@ class NewViewState extends State<NewView> with StateMother {
             thickness: 1,
           ),
           Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Gap(10),
               Expanded(
@@ -46,9 +43,9 @@ class NewViewState extends State<NewView> with StateMother {
                   // 최대 확장될 수 있는 줄 수
                   decoration: InputDecoration(
                     hintMaxLines: 1,
-                    hintText: 'Message',
+                    hintText: 'write a message...',
                     contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                     // Padding 조절
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25.0),
@@ -75,60 +72,34 @@ class NewViewState extends State<NewView> with StateMother {
               ),
               Gap(10),
               Container(
-                width: 38,
-                height: 38,
+                width: 48,
+                height: 48,
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: enableSendButton
                       ? Theme.of(context)
-                          .textTheme
-                          .bodySmall!
-                          .color!
-                          .withOpacity(0.8)
+                      .textTheme
+                      .bodySmall!
+                      .color!
+                      .withOpacity(0.8)
                       : Colors.grey.withOpacity(0.5),
                 ),
-                child: SvgPicture.asset(
-                  'assets/view/chatgpt_view/arrow-up.svg',
-                  colorFilter: ColorFilter.mode(
-                    Colors.white,
-                    BlendMode.srcIn,
-                  ),
-                ).padding(all: 7),
+                child: Icon(
+                  Icons.send_rounded,
+                  color: Colors.white,
+                ),
               ),
             ],
           ).padding(horizontal: 10, vertical: 10),
         ],
       ),
       child: Scaffold(
-        // backgroundColor: const Color(0xff202123),
-        appBar: AppbarView(),
-        drawer: DrawerView(),
-        body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          ChatCardView(
-            id: '1',
-            message: 'Hello, how can I help you?',
-            userId: 'chatGPT',
-            userName: 'ChatGPT',
-            imageUrl: 'assets/chatgpt_logo.webp',
-            createdAt: 1628180000,
-          ),
-          ChatCardView(
-            id: '2',
-            message: 'I need help with my computer',
-            userId: 'user',
-            userName: 'User',
-            imageUrl: 'assets/user.webp',
-            createdAt: 1628180000,
-          ),
-          ChatCardView(
-            id: '3',
-            message: 'Sure, what seems to be the problem?',
-            userId: 'chatGPT',
-            userName: 'ChatGPT',
-            imageUrl: 'assets/chatgpt_logo.webp',
-            createdAt: 1628180000,
-          ),
-        ]),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Text("NewView")
+            .textStyle(Theme.of(context).textTheme.displaySmall!)
+            .fontWeight(FontWeight.bold)
+            .textAlignment(TextAlign.center)
+            .center(),
         floatingActionButton: FloatingActionButtonKit(),
       ),
     );
